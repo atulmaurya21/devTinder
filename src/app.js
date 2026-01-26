710ccdf12accb9e1a35946b604014e623f18f0a6
Atul Maurya <atul.maurya@qualtechedge.com>
2026-01-26 23:40:58 +0530

    dynamic-signup

--- src/app.js
+++ src/app.js
@@ -3,22 +3,30 @@ const { connectDB } = require("./config/database");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json())
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Sachin",
-    lastName: "Tendulkar ",
-    emailId: "sachin210903@gamil.com",
-    password: "atul@123",
-  };
-  // create a new instance of User model
-  //dummy  hardcoded to data
-  const user = new User(userObj);
+
+ //js object
+  console.log(req.body);
+  const user = new User(req.body)
+  
+  // const userObj = {
+  //   firstName: "Sachin",
+  //   lastName: "Tendulkar ",
+  //   emailId: "sachin210903@gamil.com",
+  //   password: "atul@123",
+  // };
+  // // create a new instance of User model
+  // //dummy  hardcoded to data
+  // const user = new User(userObj);
   try {
     await user.save();
     res.send("User added Successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user"+ err.message)
   }
+
 });
 
 connectDB()
