7e67737c5ca64a88e9a1804cece4dada00feaf49
Atul Maurya <atul.maurya@qualtechedge.com>
2026-01-26 13:29:31 +0530

    handler for all route

--- src/app.js
+++ src/app.js
@@ -3,17 +3,35 @@ console.log("Server start ");
 
 const app = express();
 
+
+// app.use("/user", (req, res) => {
+//     res.send("every handler")
+// })
+
+// This will only handle get call to /user
+app.get("/user", (req, res) => {
+    res.send({
+      firstName: "Atul ",
+      lastName: "Kumar Maurya",
+    });
+})
+
+app.post("/user", (req, res) => {
+    console.log("Save data to database");
+    
+  res.send("Data successfully saved to the database");
+});
+
+
+app.use("/user", (req, res) => {
+    res.send("Data is sucessfully deleted");
+    
+})
+//this will match all the http method Api calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello from the server" )
 })
 
-app.use("/hello", (req, res) => {
-  res.send("Hello World");
-});
-
-app.use("/", (req, res) => {
-  res.send("All wildcard route");
-});
 
 app.listen(3000, () => {
     console.log("server is successfully  listening on port no 3000");
