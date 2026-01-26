300f8acc629ebe05f36e9ff58b32a14b8971ecf8
Atul Maurya <atul.maurya@qualtechedge.com>
2026-01-26 12:21:18 +0530

    wildcard match route

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ app.use("/test",(req, res) => {
     res.send("Hello from the server" )
 })
 
-// app.use("/hello", (req, res) => {
-//   res.send("Hello World");
-// });
+app.use("/hello", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/", (req, res) => {
+  res.send("All wildcard route");
+});
 
 app.listen(3000, () => {
     console.log("server is successfully  listening on port no 3000");
